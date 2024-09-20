8fb257d38cc8b8c16b16569d8ec4515e77b72af0
Danil Kostin <danilkostin150705@gmail.com>
2024-09-20 12:38:14 +0300

    feat(task-tracker): add types to task component

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
+import { ITask } from "../../types/Task.types";
 
-interface TaskProps {}
+interface ITaskProps extends ITask {
+  onDelete: () => void
+};
 
-interface TaskState {}
+type ITaskState = {
+  isToggled: boolean;
+};
 
-class Task extends Component<TaskProps, TaskState> {
+class Task extends Component<ITaskProps, ITaskState> {
+  constructor(props: ITaskProps) {
+    super(props);
+    this.state = {
+      isToggled: this.props.completed
+    };
+  }
   render(): React.ReactNode {
-    return <></>;
+    return (<div className="task">
+      <span>{this.props.id}</span>
+      <span>{this.props.title}</span>
+      <input type="checkbox" name="completed" id="completed" checked={this.state.isToggled} />
+    </div>);
   }
 }
 
 export default Task;
-
