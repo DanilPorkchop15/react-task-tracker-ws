ce66edbf993e5a1b2c5dba3e0d8269fa0b0e97c2
Danil Kostin <danilkostin150705@gmail.com>
2024-09-20 16:55:40 +0300

    feat(task-tracker): add userFetch service

--- src/services/User.service.ts
+++ src/services/User.service.ts
@@ -5,11 +5,28 @@ export async function fetchUsers(): Promise<IUser[]> {
   try {
     const res: Response = await fetch(`${BASE_URL}/users`);
     if (res.ok) {
-      return res.json();
+      return res.json() as Promise<IUser[]>;
+    } else {
+      console.log("Users fetch error", res.status, res.statusText);
+      throw new Error(
+        `Failed to fetch users: ${res.status} ${res.statusText}`
+      );
+    }
+  } catch (e) {
+    console.log("Users fetch error", e);
+    throw e;
+  }
+}
+
+export async function fetchUser(userId: number): Promise<IUser> {
+  try {
+    const res: Response = await fetch(`${BASE_URL}/users/${userId}`);
+    if (res.ok) {
+      return res.json() as Promise<IUser>;
     } else {
       console.log("User fetch error", res.status, res.statusText);
       throw new Error(
-        `Failed to fetch users: ${res.status} ${res.statusText}`
+        `Failed to fetch user: ${res.status} ${res.statusText}`
       );
     }
   } catch (e) {
