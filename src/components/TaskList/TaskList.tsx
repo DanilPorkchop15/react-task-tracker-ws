7a6c15238f1482f097edeb25ed5763c3ab6a4a51
Danil Kostin <danilkostin150705@gmail.com>
2024-09-20 12:39:36 +0300

    feat(task-tracker): add types to TaskList component

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
+import { ITask } from "../../types/Task.types";
+import Task from "../Task/Task";
 
-interface TaskListProps {}
+interface ITaskProps {
+  tasks: ITask[];
+  onDelete: () => void;
+}
 
-interface TaskListState {}
+interface ITaskState {}
 
-class TaskList extends Component<TaskListProps, TaskListState> {
+class TaskList extends Component<ITaskProps, ITaskState> {
+  private handleDelete () {
+    this.props.onDelete()
+  }
   render(): React.ReactNode {
-    return <></>;
+    const { tasks } = this.props;
+    return (
+      <>
+        tasks && {tasks.map((task) => (
+          <Task
+            key={task.id}
+            id={task.id}
+            title={task.title}
+            userId={task.userId}
+            completed={task.completed}
+            onDelete={this.handleDelete}
+          />
+        ))}
+      </>
+    );
   }
 }
 
