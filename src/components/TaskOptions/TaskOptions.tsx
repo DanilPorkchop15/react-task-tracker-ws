f3009cf2d2cdddf3648060e47fcbdf8c1b4c3f68
Danil Kostin <danilkostin150705@gmail.com>
2024-09-20 13:10:49 +0300

    feat(task-tracker): add types to TaskOptions component

--- src/components/TaskOptions/TaskOptions.tsx
+++ src/components/TaskOptions/TaskOptions.tsx
@@ -1,14 +1,42 @@
-import React, { Component } from "react";
+import React, { ChangeEvent, Component } from "react";
+import { ITask } from "../../types/Task.types";
+import { IUser } from "../../types/User.types";
 
-interface TaskOptionsProps {}
+interface ITaskProps {
+  onAdd: (value: string, userId: number) => void;
+}
 
-interface TaskOptionsState {}
+interface ITaskState {
+  value: string;
+  user: IUser
+}
 
-class TaskOptions extends Component<TaskOptionsProps, TaskOptionsState> {
+class TaskOptions extends Component<ITaskProps, ITaskState> {
+  constructor(props: ITaskProps) {
+    super(props);
+    this.state = {
+      value: '',
+      user: {} as IUser
+    }
+  }
+
+  private handleAdd() {
+    if (this.state.value !== "") {
+      this.props.onAdd(this.state.value, this.state.user.id)
+      this.setState({...this.state, value: ''});
+    }
+  }
+  private handleChange(e: ChangeEvent<HTMLInputElement>) {
+    this.setState({...this.state, value: e.target.value });
+  }
   render(): React.ReactNode {
-    return <></>;
+    return (
+      <div className="task-options">
+        <input type="text" />
+        <button onClick={this.handleAdd}>Add new task</button>
+      </div>
+    );
   }
 }
 
 export default TaskOptions;
-
