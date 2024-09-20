92fdd974ebba23803c09ab2d53a46f2cbd6e1c1c
Danil Kostin <danilkostin150705@gmail.com>
2024-09-20 13:11:17 +0300

    feat(task-tracker): add types to TaskTracker component

--- src/components/TaskTracker/TaskTracker.tsx
+++ src/components/TaskTracker/TaskTracker.tsx
@@ -1,18 +1,49 @@
 import React, { Component } from "react";
 import TaskList from "../TaskList/TaskList";
 import TaskOptions from "../TaskOptions/TaskOptions";
+import { CreateTaskType, ITask } from "../../types/Task.types";
 
-interface TaskTrackerProps {}
+interface ITaskProps {}
 
-interface TaskTrackerState {}
+interface ITaskState {
+  tasks: ITask[];
+}
 
-class TaskTracker extends Component<TaskTrackerProps, TaskTrackerState> {
+class TaskTracker extends Component<ITaskProps, ITaskState> {
+  private async addTask(task: CreateTaskType): Promise<ITask> {
+    const res: Response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos",
+      {
+        method: "POST",
+        body: JSON.stringify({ title: "foo", completed: false, userId: 1 }),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      }
+    );
+    return res.json() as Promise<ITask>;
+  }
+  private handleAdd(title: string, userId: number) {
+    const updatedTasks: ITask[] = this.state.tasks.slice();
+    const newTask: CreateTaskType = {
+      title,
+      userId,
+      completed: false,
+    };
+    this.addTask(newTask)
+      .then((res: ITask) => {
+        updatedTasks.push(res);
+        this.setState({ tasks: updatedTasks });
+      })
+      .catch((e: Error) => {
+        alert("Возникла ошибка при добавлении задачи!!!");
+        console.log(e);
+      });
+  }
   render(): React.ReactNode {
     return (
       <>
         <h1>React Task Tracker</h1>
-        <TaskOptions />
-        <TaskList />
+        <TaskOptions onAdd={this.handleAdd} />
+        <TaskList tasks={[]} onDelete={() => {}} />
       </>
     );
   }
