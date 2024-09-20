44b40885cfa32fe1f7afc413add7ecf4fb4f55e2
Danil Kostin <danilkostin150705@gmail.com>
2024-09-20 11:20:06 +0300

    feat(task-tracker): crearte TaskTracker component layout structure

--- src/components/TaskTracker/TaskTracker.tsx
+++ src/components/TaskTracker/TaskTracker.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import TaskList from "../TaskList/TaskList";
+import TaskOptions from "../TaskOptions/TaskOptions";
 
 interface TaskTrackerProps {}
 
@@ -6,7 +8,13 @@ interface TaskTrackerState {}
 
 class TaskTracker extends Component<TaskTrackerProps, TaskTrackerState> {
   render(): React.ReactNode {
-    return <></>;
+    return (
+      <>
+        <h1>React Task Tracker</h1>
+        <TaskOptions />
+        <TaskList />
+      </>
+    );
   }
 }
 
